e24c4f2b51b29cb27c8c24d497a6db6f4a11dd89
austinjump-sec <jerrysturgis5@gmail.com>
2026-09-19 20:53:10 -0400

    fuck

--- src/app/index.js
+++ src/app/index.js
@@ -50,6 +50,17 @@ const formatTime = (time, useAmPm) => {
 
   return `${tableEntry[1].replace(':', `:${minutes} `)}`;
 };
+
+const weekdayNumbers = {
+  Sunday: 1,
+  Monday: 2,
+  Tuesday: 3,
+  Wednesday: 4,
+  Thursday: 5,
+  Friday: 6,
+  Saturday: 7,
+};
+
 const POSSIBLE_DAYS = [
   'Sunday',
   'Monday',
--- src/components/app-tabs.tsx
+++ src/components/app-tabs.tsx
@@ -5,13 +5,14 @@ import { Colors } from '@/constants/theme';
 
 export default function AppTabs() {
   const scheme = useColorScheme();
-  const colors = Colors[scheme === 'unspecified' ? 'light' : scheme];
+  const colors = Colors[scheme === 'dark' ? 'dark' : 'light'];
 
   return (
-    <NativeTabs
-      backgroundColor={colors.background}
-      indicatorColor={colors.backgroundElement}
-      labelStyle={{ selected: { color: colors.text } }}>
+  <NativeTabs.Container 
+    backgroundColor={colors.background}
+    indicatorColor={colors.backgroundElement}
+    labelStyle={{ selected: { color: colors.text } }}
+  >
       <NativeTabs.Trigger name="index">
         <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
         <NativeTabs.Trigger.Icon
@@ -27,6 +28,6 @@ export default function AppTabs() {
           renderingMode="template"
         />
       </NativeTabs.Trigger>
-    </NativeTabs>
+       </NativeTabs.Container> 
   );
 }
